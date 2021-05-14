81874b5a351e3eef8b9394d64768702fc0c4ccc2
thejudge <thejudge@fortress.ch>
2021-05-14 17:05:32 +0200

    outputs occuring change of availability on stdout

---
+++ corona-datechecker.py
@@ -0,0 +1,34 @@
+import bs4
+import requests
+import re
+import hashlib
+import time
+
+def getAvailabilityString():
+    url="https://www.gef.be.ch/gef/de/index/Corona/Corona/corona_impfung_bern.html"
+
+    page=requests.get(url).content
+
+    soup = bs4.BeautifulSoup(page, 'lxml')
+
+
+    # find elements that contain the data we want
+    found = soup.find_all("h3", string=re.compile("Sind Termine verfügbar"))
+
+    result = ''
+    for x in found:
+        result = result + str(x.find_next_sibling('p'))
+    return result
+
+result = getAvailabilityString()
+initialString = hashlib.sha224(result.encode()).hexdigest()
+print('Initial text: ' + result)
+while True:
+    time.sleep(600)
+    result = getAvailabilityString()
+    if initialString != hashlib.sha224(result.encode()).hexdigest():
+        print('Availability has changed: ' + result)
+        initialString = hashlib.sha224(result.encode()).hexdigest()
+
+
+
