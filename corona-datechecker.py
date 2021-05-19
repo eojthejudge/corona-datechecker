df21236231ba0a280ba7b7995397b32202ee4c35
thejudge <thejudge@fortress.ch>
2021-05-19 17:10:46 +0200

    first working version with matrix messaging and event loop

--- corona-datechecker.py
+++ corona-datechecker.py
@@ -2,7 +2,10 @@ import bs4
 import requests
 import re
 import hashlib
-import time
+import credentials
+from nio import AsyncClient
+import asyncio
+
 
 def get_availability_string():
     url="https://www.gef.be.ch/gef/de/index/Corona/Corona/corona_impfung_bern.html"
@@ -20,15 +23,42 @@ def get_availability_string():
         result = result + str(x.find_next_sibling('p'))
     return result
 
-result = get_availability_string()
-initialString = hashlib.sha224(result.encode()).hexdigest()
-print('Initial text: ' + result)
-while True:
-    time.sleep(600)
-    result = get_availability_string()
-    if initialString != hashlib.sha224(result.encode()).hexdigest():
-        print('Availability has changed: ' + result)
-        initialString = hashlib.sha224(result.encode()).hexdigest()
+
+async def send_message(message, client):
+    await client.login(credentials.login['password'])
+    await client.room_send(
+        room_id="!WpWbonOZKBOYVPWXkQ:matrix.fortress.ch",
+        message_type="m.room.message",
+        content = {
+            "msgtype": "m.text",
+            "body": message
+        }
+    )
+
+async def main():
+    result = get_availability_string() + "first"
+    initial_string = hashlib.sha224(result.encode()).hexdigest()
+    client = AsyncClient("https://matrix.fortress.ch", "@coronabot:matrix.fortress.ch")
+    print('Initial text: ' + result)
+    await send_message('Initial text: ' + result, client)
+    while True:
+        await asyncio.sleep(60)
+        result = get_availability_string()
+        if initial_string != hashlib.sha224(result.encode()).hexdigest():
+            print('Availability has changed: ' + result)
+            await send_message('Availability has changed: ' + result, client)
+            initial_string = hashlib.sha224(result.encode()).hexdigest()
+
+loop = asyncio.get_event_loop()
+try:
+    asyncio.ensure_future(main())
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    print("Closing Loop")
+    loop.close()
+
 
 
 
